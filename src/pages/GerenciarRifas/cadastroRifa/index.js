8205acde8c4e9de570678edefb23bee0f01b0f73
L0H4NX <lsteferson83@gmail.com>
2023-03-16 22:57:26 -0300

    Feat - criar bilhetes da rifa

--- src/pages/GerenciarRifas/cadastroRifa/index.js
+++ src/pages/GerenciarRifas/cadastroRifa/index.js
@@ -53,7 +53,6 @@ function CadastrarRifa() {
         valorBilhete,
         numeroInicial,
         numeroFinal,
-        priority: false,
       });
       alert('Cadastro realizado com sucesso!');
     }
