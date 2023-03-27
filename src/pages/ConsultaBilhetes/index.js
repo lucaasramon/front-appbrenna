5d5bef158e3a100f10bf868b31e439c4a55c1195
jntvsc2 <jntvsc@hotmail.com>
2023-03-27 09:08:44 -0300

    Acréscimo do select de Rifas na ConsultaBilhetes

--- src/pages/ConsultaBilhetes/index.js
+++ src/pages/ConsultaBilhetes/index.js
@@ -9,25 +9,37 @@ import { Link } from "react-router-dom";
 function ConsultaBilhetes() {
   const [bilhetesAll, setBilhetesAll] = useState([]);
   const [equipesAll, setEquipesAll] = useState([]);
+  const [equipesSelecionada, setEquipesSelecionadas] = useState([]);
   const [equipesItem, setItemEquipe] = useState("");
   const [bilhetesEquipe, setBilhetesEquipe] = useState([]);
   const [idBilhete, setIdBilhete] = useState(null);
+  const [allRifas, setAllRifas] = useState([]);
+  const [rifaId, setRifa] = useState("");
 
-  console.log("TESTE", idBilhete);
+  //Obtem a rifa selecionada:
+  function onChangeRifa(e) {
+    const _selectedValue = e.target.value;
+    setRifa(_selectedValue);
+    //console.log(("RIFA SELECIONADA",{_selectedValue, rifaId}));
+    //setItemEquipe("Todas");
+
+    //
+    if (_selectedValue !== "") {
+      setEquipesSelecionadas(equipesAll.filter(function (e) {return e.rifa_id === parseInt(_selectedValue)}));      
+    }
+  }
+
   //Obtem a equipe selecionada:
-  function setEquipeItem(e) {
+  function onChangeEquipe(e) {
     const selectedValue = e.target.value;
     setItemEquipe(selectedValue);
-
-    console.log(selectedValue);
-    if (selectedValue !== "Todas") {
-      //Obtem os bilhetes por equipe selecionadas:
-      setBilhetesEquipe(
-        bilhetesAll.filter(function (e) {
-          return e.equipe === selectedValue;
-        })
-      );
-    }
+    
+    //Obtem os bilhetes por equipe selecionadas:
+    setBilhetesEquipe(
+      bilhetesAll.filter(function (e) {
+        return e.equipe === selectedValue && e.rifa_id === rifaId        
+      }) 
+    );    
   }
 
   useEffect(() => {
@@ -38,14 +50,23 @@ function ConsultaBilhetes() {
     }
     getBilhetes();
 
-    setItemEquipe("Todas");
+    //setItemEquipe("Todas");
+    setRifa("");
 
     async function getEquipes() {
       const responseEquipes = await api.get("/equipes");
 
       setEquipesAll(responseEquipes.data);
+      //setEquipesSelecionadas(responseEquipes.data);
     }
     getEquipes();
+
+    async function retornaRifas() {
+      const rifas = await api.get("/rifas");
+      setAllRifas(rifas.data);
+    }
+    retornaRifas();
+
   }, []);
 
   return (
@@ -61,7 +82,22 @@ function ConsultaBilhetes() {
           <label for="">Filtrar</label>
         </div>
         <hr />
-        <label class="active" for="">
+        <div className="col s12 m6">
+          <label className="active" for="rifa">Rifa:</label>
+          <select            
+            id="rifa"
+            type="text"
+            value={rifaId}
+            onChange={onChangeRifa}
+            className="input-field browser-default"
+          >
+            <option value="">Selecione...</option>
+            {allRifas.map((data) => (
+              <option value={data.rifa_id}>{data.titulo}</option>
+            ))}
+          </select>
+        </div>
+        <label class="active" for="equipeSelecionada">
           Equipes:
         </label>
         <div className="col s12 m6">
@@ -69,11 +105,11 @@ function ConsultaBilhetes() {
             id="equipeSelecionada"
             type="submit"
             value={equipesItem}
-            onChange={setEquipeItem}
+            onChange={onChangeEquipe}
             className="input-field browser-default"
           >
-            <option value="Todas">Todas...</option>
-            {equipesAll.map((data) => (
+            <option value="">Selecione...</option>
+            {equipesSelecionada.map((data) => (
               <option value={data.equipe}>{data.equipe}</option>
             ))}
           </select>
@@ -81,49 +117,28 @@ function ConsultaBilhetes() {
         <div class="container">
           <div class="section">
             <div>
-              <h6 class="sec-tit">Equipe: {equipesItem}</h6>
+              <h6 class="sec-tit"><b>Bilhetes da equipe:</b> {equipesItem}</h6>
             </div>
-            <div className="conteiner">
-              {equipesItem === "Todas"
-                ? bilhetesAll.map((data) => (
-                    <div
-                      onClick={() => {
-                        setIdBilhete(data._id);
-                        if(data.status){
-                          alert("Está sendo feito uma tela para vizualizar os detalhes da venda.")
-                        }else{
-                          window.location.href = `/cadastro-Bilhete/${data._id}`;
-                        }
-                      }}
-                      className="conteinerBilhetes"
-                    >
-                      <BoxBilhete
-                        numero={data.bilhete}
-                        data={data.bilheteVenda[0].dataVenda}
-                        status={data.status}
-                      />
-                    </div>
-                  ))
-                : (console.log(bilhetesEquipe),
-                  bilhetesEquipe.map((data) => (
-                    <div
-                      onClick={() => {
-                        setIdBilhete(data._id)
-                        if(data.status){
-                          alert("Está sendo feito uma tela para vizualizar os detalhes da venda.")
-                        }else{
-                          window.location.href = `/cadastro-Bilhete/${data._id}`;
-                        }
-                      }}
-                      className="conteinerBilhetes"
-                    >
-                      <BoxBilhete
-                        numero={data.bilhete}
-                        data={data.bilheteVenda[0].dataVenda}
-                        status={data.status}
-                      />
-                    </div>
-                  )))}
+            <div className="conteiner">                
+              {bilhetesEquipe.map((data) => (
+                <div
+                  onClick={() => {
+                    setIdBilhete(data._id)
+                    if(data.status){
+                      alert("Está sendo feito uma tela para vizualizar os detalhes da venda.")
+                    }else{
+                      window.location.href = `/cadastro-Bilhete/${data._id}`;
+                    }
+                  }}
+                  className="conteinerBilhetes"
+                >
+                  <BoxBilhete
+                    numero={data.bilhete}
+                    data={data.bilheteVenda[0].dataVenda}
+                    status={data.status}
+                  />
+                </div>   
+              ))}
             </div>
           </div>
         </div>
