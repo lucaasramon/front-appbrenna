c70a0f2ea4f71f40cd7e595c32b3e19930765d47
jntvsc2 <jntvsc@hotmail.com>
2023-03-28 16:14:06 -0300

    Alteração variável porta

--- src/server/api.js
+++ src/server/api.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 require("dotenv").config();
 
-console.log(process.env.REACT_APP_BASE_URL)
 const api = axios.create({
   //DESENVOLVIMENTO:
   baseURL: process.env.REACT_APP_BASE_URL,
-
 });
 
 export default api;
